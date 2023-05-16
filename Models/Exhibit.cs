72595aad61e33a906e6494b4fb53163aa52e669d
Harun Hursid <harun.hursid@hotmail.com>
2023-05-16 12:58:05 +0200

    tilføjede fejl beskeder

--- Models/Exhibit.cs
+++ Models/Exhibit.cs
@@ -10,8 +10,16 @@ namespace RagnarockApp.Models
     {
 
         public int Id { get; set; } 
+
+        [Display(Name = "Exhibition name")]
+        [Required(ErrorMessage = "Name of Exhibition is required"), MaxLength(150)]
+        
         public string Name { get; set; }
+        
+        [Display(Description = "Description of Exhibition")]
+        [Required(ErrorMessage = "Descriptionen of Exhibition is required"), MaxLength(500)]
         public string Description { get; set; }
+        
         public string Imagename { get; set; }
 
     }
