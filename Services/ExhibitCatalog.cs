76dc0c2468adedc5d121eab550e5bad132f187a8
Harun Hursid <harun.hursid@hotmail.com>
2023-05-21 13:29:01 +0200

    tilføjet billeder

--- Services/ExhibitCatalog.cs
+++ Services/ExhibitCatalog.cs
@@ -11,11 +11,11 @@ namespace RagnarockApp.Services
         public ExhibitCatalog() 
         { 
             Exhibits = new Dictionary<int,Exhibit>();
-            Exhibits.Add(1, new Exhibit() {Id = 1,Name = "Exhibit 1", Description = "guitar 1", Imagename = "billede1"});
-            Exhibits.Add(2, new Exhibit() {Id = 2,Name = "Exhibit 2", Description = "guitar 2", Imagename = "billede2"});
-            Exhibits.Add(3, new Exhibit() {Id = 3,Name = "exhibit 3", Description = "guitar 3", Imagename = "billede3"});
-            Exhibits.Add(4, new Exhibit() {Id = 4,Name = "exhibit 4", Description = "guitar 4", Imagename = "billede4"});
-            Exhibits.Add(5, new Exhibit() {Id = 5,Name = "exhibit 5", Description = "guitar 5", Imagename = "billede5" });
+            Exhibits.Add(1, new Exhibit() {Id = 1,Name = "Exhibit 1", Description = "guitar 1", Imagename = "Guitarist.JPEG"});
+            Exhibits.Add(2, new Exhibit() {Id = 2,Name = "Exhibit 2", Description = "guitar 2", Imagename = "RockBand.JPEG"});
+            Exhibits.Add(3, new Exhibit() {Id = 3,Name = "exhibit 3", Description = "guitar 3", Imagename = "RockBand2.JPEG"});
+            Exhibits.Add(4, new Exhibit() {Id = 4,Name = "exhibit 4", Description = "guitar 4", Imagename = "PopBandSinger.JPEG"});
+            Exhibits.Add(5, new Exhibit() {Id = 5,Name = "exhibit 5", Description = "guitar 5", Imagename = "PopBand.JPEG" });
 
         }
 
