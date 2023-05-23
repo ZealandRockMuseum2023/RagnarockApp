206e46f6b82cb115fcb7cd4fb8516fc08a1af137
Harun Hursid <harun.hursid@hotmail.com>
2023-05-23 12:57:39 +0200

    vores json commands

---
+++ Helpers/JsonFileReader.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using RagnarockApp.Models;
+
+namespace RagnarockApp.Helpers
+{
+    public class JsonFileReader
+    {
+        public static Dictionary<int, Exhibit> ReadJson(string JsonFileName)
+        {
+            string jsonString = File.ReadAllText(JsonFileName);
+            return JsonConvert.DeserializeObject<Dictionary<int, Exhibit>>(jsonString);
+        }
+    }
+}
---
+++ Helpers/JsonFileWriter.cs
@@ -0,0 +1,17 @@
+﻿using RagnarockApp.Models;
+
+namespace RagnarockApp.Helpers
+{
+    public class JsonFileWriter
+    {
+
+        public static void WriteToJson(Dictionary<int, Exhibit> exhibit, string JsonFileName)
+        {
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(exhibit,
+                                                               Newtonsoft.Json.Formatting.Indented);
+
+            File.WriteAllText(JsonFileName, output);
+      
+        }
+    }
+}
