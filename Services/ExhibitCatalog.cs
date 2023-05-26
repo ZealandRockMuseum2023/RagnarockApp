e04f8a2765414ac1c805eb5dc65d56559424b8b5
Harun Hursid <harun.hursid@hotmail.com>
2023-05-26 11:18:07 +0200

    opdaterede sound

--- Services/ExhibitCatalog.cs
+++ Services/ExhibitCatalog.cs
@@ -11,11 +11,11 @@ namespace RagnarockApp.Services
         public ExhibitCatalog() 
         { 
             Exhibits = new Dictionary<int,Exhibit>();
-            Exhibits.Add(1, new Exhibit() {Id = 1,Name = "guitar", Description = "Guitaren bliver brugt som en af de vigtigste instrumenter i et rockband, næsten alle bands har minimum 1 guitar med ", Imagename = "Guitar.JPEG",Soundname = "Ronaldo suiii.mp3" });
-            Exhibits.Add(2, new Exhibit() {Id = 2,Name = "Blokfløjte",Description = "Blokfløjten har en fantisk lyd og man kan spille en masse melodiere på en blokføjte", Imagename = "BlokFløjte.JPEG",Soundname = "Ankara_messi.mp3.mp3" });
-            Exhibits.Add(3, new Exhibit() {Id = 3,Name = "Bass",Description = "Bass er ligesom guitaren en af de vigtiste instrumenter og har en fantastisk dyb melodi.", Imagename = "Bass.JPEG",Soundname = "funnysound.mp3.mp3" });
-            Exhibits.Add(4, new Exhibit() {Id = 4,Name = "Keyboard",Description = "Keyboard er et fantastisk instrument som kan bruges i næsten alle former for musik, man kan næsten spille alle lyde og melodier på et keyboard.", Imagename = "Keyboard.JPEG",Soundname = "shutyourmouth.mp3.mp3" });
-            Exhibits.Add(5, new Exhibit() {Id = 5,Name = "trummer",Description = "Trummerne er et af de fedeste instrumenter i musikverden, næsten alle bands har en trommeslager.", Imagename = "trummer.JPEG",Soundname = "whoisthatguy.mp3.mp3" });
+            Exhibits.Add(1, new Exhibit() {Id = 1,Name = "guitar", Description = "Guitaren bliver brugt som en af de vigtigste instrumenter i et rockband, næsten alle bands har minimum 1 guitar med ", Imagename = "Guitar.JPEG",Soundname = "Guitar.mp3" });
+            Exhibits.Add(2, new Exhibit() {Id = 2,Name = "Blokfløjte",Description = "Blokfløjten har en fantisk lyd og man kan spille en masse melodiere på en blokføjte", Imagename = "BlokFløjte.JPEG",Soundname = "Recorder.mp3" });
+            Exhibits.Add(3, new Exhibit() {Id = 3,Name = "Bass",Description = "Bass er ligesom guitaren en af de vigtiste instrumenter og har en fantastisk dyb melodi.", Imagename = "Bass.JPEG",Soundname = "Bass.mp3" });
+            Exhibits.Add(4, new Exhibit() {Id = 4,Name = "Keyboard",Description = "Keyboard er et fantastisk instrument som kan bruges i næsten alle former for musik, man kan næsten spille alle lyde og melodier på et keyboard.", Imagename = "Keyboard.JPEG",Soundname = "Piano.mp3" });
+            Exhibits.Add(5, new Exhibit() {Id = 5,Name = "trummer",Description = "Trummerne er et af de fedeste instrumenter i musikverden, næsten alle bands har en trommeslager.", Imagename = "trummer.JPEG",Soundname = "Drums.mp3" });
 
         }
 
